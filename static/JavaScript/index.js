c3d675728d07e8f30545bcedf010d33b41ca7693
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-06-24 13:35:00 -0700

    drop down menu

--- static/JavaScript/index.js
+++ static/JavaScript/index.js
@@ -1,3 +1,5 @@
-function startExercise() {
-    window.location.href = "{{url_for('first_exercise')}}";
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.navbar');
+    const navbarHeight = navbar.offsetHeight;
+    document.documentElement.style.setProperty('--navbar-height', `${navbarHeight}px`);
+});
