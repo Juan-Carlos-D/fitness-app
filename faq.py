c8dc29149dcf9b757dfdf76169df89f96e559eaf
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-06-26 16:55:46 -0700

    faq.py

---
+++ faq.py
@@ -0,0 +1,20 @@
+# FAQ Q&A
+
+faq_data = [
+    {
+        "question": "What is calisthenics?",
+        "answer": "Calisthenics is a form of strength training that utilizes an individual's body weight as resistance to perform multi-joint compound movements with little or no equipment."
+    },
+    {
+        "question": "How do I know which program is right for me?",
+        "answer": "First, decide what goals you want to accomplish. Then, pick a program that cloely aligns with that goal. If you are a beginner, it is recommended to start with the beginner program to build a foundation."
+    },
+    {
+        "question": "What equipment do I need? Can I workout at home?",
+        "answer": "You can absolutely do these workouts in the comfort of your home. Some programs may require equipment, but the program description will have the required equipments before any purchases are made."
+    },
+    {
+        "question": "How will I receive my program?",
+        "answer": ""
+    }
+]
