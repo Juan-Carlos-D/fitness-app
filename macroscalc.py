47e4a9dbcef45d202858cd56c268d03bfea52efb
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-06-28 15:49:35 -0700

    macrocalc

--- macroscalc.py
+++ macroscalc.py
@@ -1,14 +1,19 @@
-def calculate_macros(weight, height, age, gender, activity_level, goal, weight_unit, height_unit):
+def calculate_macros(weight, height, age, gender, activity_level, goal, weight_unit='kg', height_unit='cm'):
+    # Convert weight to kg if in pounds
     if weight_unit == 'lbs':
-        weight = weight * 0.453592  # Convert lbs to kg
-    if height_unit == 'inches':
-        height = height * 2.54  # Convert inches to cm
+        weight = weight * 0.453592
 
+    # Convert height to cm if in inches
+    if height_unit == 'inches':
+        height = height * 2.54
+
+    # Calculate BMR
     if gender == 'male':
         bmr = 10 * weight + 6.25 * height - 5 * age + 5
     elif gender == 'female':
         bmr = 10 * weight + 6.25 * height - 5 * age - 161
 
+    # Activity factors
     activity_factors = {
         'sedentary': 1.2,
         'lightly_active': 1.375,
@@ -19,31 +24,27 @@ def calculate_macros(weight, height, age, gender, activity_level, goal, weight_u
 
     maintenance_calories = bmr * activity_factors[activity_level]
 
+    # Adjust calories based on goal
     if goal == 'maintain':
         calories = maintenance_calories
-        protein = 1.5 * weight
-        carbohydrates = 3.5 * weight
-        fats = 0.3 * calories / 9
     elif goal == 'gain_slow':
         calories = maintenance_calories + 250  # Gain ~0.5 lb per week
-        protein = 1.8 * weight
-        carbohydrates = 4.0 * weight
-        fats = 0.35 * calories / 9
     elif goal == 'gain_fast':
         calories = maintenance_calories + 500  # Gain ~1 lb per week
-        protein = 2.0 * weight
-        carbohydrates = 4.5 * weight
-        fats = 0.4 * calories / 9
     elif goal == 'lose_slow':
         calories = maintenance_calories - 250  # Lose ~0.5 lb per week
-        protein = 1.8 * weight
-        carbohydrates = 3.0 * weight
-        fats = 0.25 * calories / 9
     elif goal == 'lose_fast':
         calories = maintenance_calories - 500  # Lose ~1 lb per week
-        protein = 2.0 * weight
-        carbohydrates = 2.5 * weight
-        fats = 0.2 * calories / 9
+
+    # Adjust macronutrients based on goal
+    if goal in ['maintain', 'gain_slow', 'gain_fast']:
+        protein = 2.0 * weight  # Higher protein for muscle gain
+        fats = 0.3 * calories / 9
+        carbohydrates = (calories - (protein * 4 + fats * 9)) / 4
+    else:  # For weight loss
+        protein = 2.2 * weight  # Higher protein to preserve muscle
+        fats = 0.25 * calories / 9
+        carbohydrates = (calories - (protein * 4 + fats * 9)) / 4
 
     return {
         'calories': round(calories),
