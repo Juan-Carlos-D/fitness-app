054b5199dd1427692bb999dcf2bb2b29a460c6dc
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-07-12 21:36:45 -0700

    js

--- static/JavaScript/gallery.js
+++ static/JavaScript/gallery.js
@@ -1,27 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.gallery-container img');
-    const prevButton = document.querySelector('.nav-button.prev');
-    const nextButton = document.querySelector('.nav-button.next');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const images = document.querySelectorAll('.gallery-container img');
+//     const prevButton = document.querySelector('.nav-button.prev');
+//     const nextButton = document.querySelector('.nav-button.next');
 
-    let currentIndex = 0;
+//     let currentIndex = 0;
 
-    function showImage(index) {
-        images.forEach(img => img.style.display = 'none');
-        images[index].style.display = 'block';
+//     function showImage(index) {
+//         images.forEach(img => img.style.display = 'none');
+//         images[index].style.display = 'block';
+//     }
+
+//     function changeImage(direction) {
+//         currentIndex = (currentIndex + direction + images.length) % images.length;
+//         showImage(currentIndex);
+//     }
+
+//     prevButton.addEventListener('click', () => {
+//         changeImage(-1);
+
+//     nextButton.addEventListener('click', () => {
+//         changeImage(1); 
+//     });
+
+//     showImage(currentIndex); 
+// });
+
+
+var counter = 1;
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4) {
+        counter = 1;
     }
-
-    function changeImage(direction) {
-        currentIndex = (currentIndex + direction + images.length) % images.length;
-        showImage(currentIndex);
-    }
-
-    prevButton.addEventListener('click', () => {
-        changeImage(-1); // Previous image
-    });
-
-    nextButton.addEventListener('click', () => {
-        changeImage(1); // Next image
-    });
-
-    showImage(currentIndex); // Initial display
-});
+}, 5000);
