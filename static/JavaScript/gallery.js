308320b478a44d1478e4a7cb1dddcb43955b865d
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-07-12 22:14:17 -0700

    slider somewhat complete - will make adjusments over the wknd

--- static/JavaScript/gallery.js
+++ static/JavaScript/gallery.js
@@ -1,31 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const images = document.querySelectorAll('.gallery-container img');
-//     const prevButton = document.querySelector('.nav-button.prev');
-//     const nextButton = document.querySelector('.nav-button.next');
-
-//     let currentIndex = 0;
-
-//     function showImage(index) {
-//         images.forEach(img => img.style.display = 'none');
-//         images[index].style.display = 'block';
-//     }
-
-//     function changeImage(direction) {
-//         currentIndex = (currentIndex + direction + images.length) % images.length;
-//         showImage(currentIndex);
-//     }
-
-//     prevButton.addEventListener('click', () => {
-//         changeImage(-1);
-
-//     nextButton.addEventListener('click', () => {
-//         changeImage(1); 
-//     });
-
-//     showImage(currentIndex); 
-// });
-
-
 var counter = 1;
 setInterval(function() {
     document.getElementById('radio' + counter).checked = true;
@@ -33,4 +5,4 @@ setInterval(function() {
     if(counter > 4) {
         counter = 1;
     }
-}, 5000);
+}, 3500);
